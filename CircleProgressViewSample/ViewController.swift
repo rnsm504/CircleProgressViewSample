47a3ec7fcf4d2ef972fd88cabd16e1170fdccae4
Kuze Masanori <kuze@MacBookPro.local>
2015-01-14 01:45:07 +0900

    Initial Commit

---
+++ CircleProgressViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleProgressViewSample
+//
+//  Created by Kuze Masanori on 2015/01/14.
+//  Copyright (c) 2015年 Kuze Masanori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
